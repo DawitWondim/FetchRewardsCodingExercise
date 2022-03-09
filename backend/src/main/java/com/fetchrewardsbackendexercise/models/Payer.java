f2b7e3e4caae8b0a6f8d97099a4f9ea95ecccc5a
Dawit Wondim <dawit346@gmail.com>
2022-03-08 17:18:44 -0800

    Added the models/entities

---
+++ backend/src/main/java/com/fetchrewardsbackendexercise/models/Payer.java
@@ -0,0 +1,32 @@
+package main.java.com.fetchrewardsbackendexercise.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "PAYER")
+public class Payer {
+
+    @Column(name = "id")
+    public @Id @GeneratedValue Integer id;
+
+    @Column(name = "name")
+    public String name;
+
+    @Column(name = "points")
+    public int points;
+
+    public Payer() {
+    }
+
+    public Payer(String name, int points) {
+        this.name = name;
+        this.points = points;
+    }
+}
---
+++ backend/src/main/java/com/fetchrewardsbackendexercise/models/Transaction.java
@@ -0,0 +1,39 @@
+package main.java.com.fetchrewardsbackendexercise.models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TRANSACTION")
+public class Transaction {
+
+    @Column(name = "transaction_id")
+    public @Id @GeneratedValue Integer id;
+
+    @Column(name = "payer")
+    public String payer;
+
+    @Column(name = "points")
+    public int points;
+
+    @Column(name = "timestamp")
+    public Timestamp timestamp;
+
+    public Transaction() {
+    }
+
+    public Transaction(String payer, int points, Timestamp timestamp) {
+        this.payer = payer;
+        this.points = points;
+        this.timestamp = timestamp;
+    }
+
+}
