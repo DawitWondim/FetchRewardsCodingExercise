2ff705eeaabffcae5cf0b3b73b9367f3767c07de
Dawit Wondim <dawit346@gmail.com>
2022-03-15 12:11:44 -0700

    Project is complete!

--- backend/src/main/java/com/fetchrewardsbackendexercise/controller/RewardsController.java
+++ backend/src/main/java/com/fetchrewardsbackendexercise/controller/RewardsController.java
@@ -3,6 +3,7 @@ package main.java.com.fetchrewardsbackendexercise.controller;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -15,7 +16,7 @@ import main.java.com.fetchrewardsbackendexercise.repositories.PayerRepository;
 import main.java.com.fetchrewardsbackendexercise.repositories.TransactionRepository;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,7 +25,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 public class RewardsController {
 
     PayerRepository payerRepository;
@@ -35,28 +35,49 @@ public class RewardsController {
         this.transactionRepository = transactionRepository;
     }
 
+    /**
+     *
+     * @return
+     *
+     * This endpoint handles the "RETURN ALL PAYER POINT BALANCES" functionality. The Spring Data JPA framework
+     * provides methods signatures that work as queries to the SQL database. Thus, the findAll() method will
+     * retrieve all of the payers in the Payers table. Each Payer model contains the Payer's name and their
+     * respective points.
+     */
     @GetMapping("/payers")
     Collection<Payer> getPayers() {
         return (Collection<Payer>) payerRepository.findAll();
     }
 
+    /**
+     *
+     * @return
+     * This endpoint was not required for the project but it may come in handy in POSTMAN or some other API
+     * testing tool to verify the transactions in the database. It will return the entire list of records in
+     * the transactions table.
+     */
     @GetMapping("/transactions")
     Collection<Transaction> getTransactions() {
         return (Collection<Transaction>) transactionRepository.findAll();
     }
 
-    @PostMapping("/payers")
-    ResponseEntity<Payer> createPayer(@Valid @RequestBody Payer payer) throws URISyntaxException {
-        Payer result = payerRepository.save(payer);
-        System.out.println(payer.name);
-        return ResponseEntity.ok().body(result);
-    }
-
+    /**
+     *
+     * @param transaction
+     * @return
+     * @throws URISyntaxException
+     *
+     *  This endpoint handles the "ADD TRANSACTIONS FOR A SPECIFIC PAYER AND DATE" functionality. The client will
+     *  send a Transaction object in JSON format that will contain the respective values for the transaction,
+     *  which are the Payer's name (String), the points for the transaction (int), and the timestamp. The
+     *  implementation below will add the Payer to the Payer table in the database if the Payer does not exist,
+     *  and it will update the Payers point balance if it does exist. Additionally, the transaction record will be
+     *  added to the transaction table.
+     */
     @PostMapping("/transactions")
     ResponseEntity<Transaction> addTransaction(@Valid @RequestBody Transaction transaction)
             throws URISyntaxException {
 
-        // Look for assert.notnull to improve below code.
         Payer payer = payerRepository.findByName(transaction.payer);
         if (null == payer) {
            if (transaction.points < 0) {
@@ -81,51 +102,108 @@ public class RewardsController {
     }
 
     int pts = 0;
-    PointsToSpend pointstospend;
-    List<PointsToSpend> pointstospendList = new ArrayList<>();
+
+    /**
+     *
+     * @param stp
+     * @return
+     * @throws URISyntaxException
+     *
+     * This endpoint handles the "Spend points using the rules above and return a
+     * list of { "payer": <string>, "points": <integer> } for each call" functionality. The client passes in
+     * a SpendThesePoints object that contains the amount of points they want to spend. The logic for
+     * implementing the functionality is to start by returning the records from the Transaction table with the records
+     * ordered by timestamp. The findByOrderByTimestamp() function will return those records in order.  Once
+     * we have the first transaction in the queue, we begin to deduct the stp (points) passed in as a parameter
+     * until it reaches 0. If the transaction amount is less than the stp (points), I would delete the transaction
+     * record from the table and move onto the next transaction. Additionally, while I spend the points, I
+     * adjust the payer's points balances in the payers table.
+     */
     @PutMapping("/spendpoints")
     Collection<PointsToSpend> spendPoints(@Valid @RequestBody SpendThesePoints stp) throws URISyntaxException {
-        
+
+        /**
+         * The pointtospendList is the object that will be returned to the client after this endpoint is called. It
+         * will return the values spent by each Payer object in the application.
+         */
+        List<PointsToSpend> pointstospendList = new ArrayList<>();
+
+        /**
+         * This HashSet will allow us to know whether or not a specific payer has been called on more than once
+         * to spend its points. A HashSet is efficient for searching as it is O(1) time complexity.
+         */
+        HashSet<String> payersInPointsToSpendList = new HashSet<>();
         pts = stp.getPoints();
-        
+
+        /**
+         * This is where I iterate through the transaction table, in order of timestamp. I have different conditions
+         * for when the transaction points is negative or positive. As described in the PDF file, the rules dictate
+         * that positive amounts would imply that the Payers are losing points and negative amounts imply that the
+         * payers are gaining points.
+         */
         transactionRepository.findByOrderByTimestamp().forEach(transaction -> {
-            boolean repeatPayer = false;
-            for (int a = 0; a < pointstospendList.size(); a++) {
-                if (transaction.payer.equals(pointstospendList.get(a).getPayer())) {
-                    repeatPayer = true;
-                    if (pts > 0) {
-                        if (transaction.points > pts) {
-                            transaction.points -= pts;
-                            pointstospendList.get(a).points += ((-1) * pts);
-                            pts = 0;
-                        } else if ((transaction.points > 0) && (transaction.points <= pts)) {
-                            pts -= transaction.points;
-                            pointstospendList.get(a).points += ((-1) * transaction.points);
-                            transactionRepository.deleteById(transaction.getId());
-                        } else {
-                            pts += transaction.points;
-                            pointstospendList.get(a).points += ((-1) * transaction.points);
-                            transactionRepository.deleteById(transaction.getId());
-                        }
-                    }
-                } 
-            }
-            if (!repeatPayer) {
-                pointstospend = new PointsToSpend();
-                if (pts > 0) {
-                    if (transaction.points > pts) {
-                        transaction.points -= pts;
-                        pts = 0;
-                    } else if ((transaction.points > 0) && (transaction.points <= pts)) {
-                        pts -= transaction.points;
-                        transactionRepository.deleteById(transaction.getId());
-                    } else {
-                        pts += transaction.points;
-                        transactionRepository.deleteById(transaction.getId());
-                    }
-                }
-                pointstospendList.add(pointstospend);
-            }
+           if (pts > 0) {
+               Payer payer = payerRepository.findByName(transaction.payer);
+
+               /**
+                * This condition handles the logic for when the transaction amount is positive. It will deduct
+                * Payer point balances and lower the pts(balance) to spend points. Additionally, it
+                * updates the pointsToSpendList that will be returned to the client. The else condition will handle
+                * the logic for when the transaction amount is negative.
+                */
+               if (transaction.points >= 0) {
+                   int temp = Math.min(transaction.points, pts);
+                   transaction.points -= temp;
+                   pts -= temp;
+                       payer.setPoints(payer.getPoints() - temp);
+                       if (payer.getPoints() < 0) payer.setPoints(0);
+                       payerRepository.delete(payer);
+                       payerRepository.save(payer);
+
+                   if (payersInPointsToSpendList.contains(transaction.getPayer())) {
+                       for (int a = 0; a < pointstospendList.size(); a++) {
+                           PointsToSpend currentPTS = pointstospendList.get(a);
+                           if (transaction.getPayer().equals(currentPTS.getPayer())) {
+                               currentPTS.setPoints(currentPTS.points -= temp);
+                               break;
+                           }
+                       }
+                   } else {
+                       payersInPointsToSpendList.add(transaction.getPayer());
+                       PointsToSpend currentPTS = new PointsToSpend(transaction.getPayer(), (-1) * temp );
+                       pointstospendList.add(currentPTS);
+                   }
+
+                   if (transaction.points == 0) {
+                       transactionRepository.deleteById(transaction.getId());
+                   }
+
+               } else {
+                   payer.setPoints(payer.getPoints() + transaction.points);
+                   pts += transaction.points;
+                   if (payersInPointsToSpendList.contains(transaction.getPayer())) {
+                       for (int a = 0; a < pointstospendList.size(); a++) {
+                           PointsToSpend currentPTS = pointstospendList.get(a);
+                           if (transaction.getPayer().equals(currentPTS.getPayer())) {
+                               currentPTS.setPoints(currentPTS.points += transaction.points);
+                               break;
+                           }
+                       }
+                   } else {
+                       payersInPointsToSpendList.add(transaction.getPayer());
+                       PointsToSpend currentPTS = new PointsToSpend(transaction.getPayer(), transaction.points);
+                       pointstospendList.add(currentPTS);
+                   }
+
+                   /**
+                    * This is where I delete a transaction record in case the transaction amount is 0.
+                    */
+                   if (transaction.points == 0) {
+                       transactionRepository.deleteById(transaction.getId());
+                   }
+               }
+           }
+
         });
         return pointstospendList;
     }
